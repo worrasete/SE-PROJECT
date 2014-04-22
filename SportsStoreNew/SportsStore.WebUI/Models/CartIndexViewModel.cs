48a6b641a4abaab9f7179a3702e067a96190f951
Worrasete Tansurat <se542115056@vr.camt.info>
2014-04-22 19:18:25 +0700

    Restart

--- SportsStoreNew/SportsStore.WebUI/Models/CartIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using SportsStore.Domain.Entities;
-namespace SportsStore.WebUI.Models
-{
-    public class CartIndexViewModel
-    {
-        public Cart Cart { get; set; }
-        public string ReturnUrl { get; set; }
-    }
-}
